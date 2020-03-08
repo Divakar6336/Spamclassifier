18e2d3677338b40b37ec154f851f1d1a05b5c177
Divakar6336 <divakar6336@gmail.com>
2020-03-08 23:58:21 +0530

    model deployment

---
+++ app.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[5]:
+
+
+from sklearn.feature_extraction.text import CountVectorizer 
+from sklearn.naive_bayes import MultinomialNB
+from flask import render_template, url_for, request, Flask
+import pickle
+
+
+# In[8]:
+
+
+clf=pickle.load(open('transform.pkl', 'rb'))
+model=pickle.load(open('model.pkl', 'rb'))
+
+app=Flask(__name__)
+@app.route('/')
+def home():
+    render_template('home.html')
+    
+@app.route('/Predict', methods=['POST'])
+def predict():
+    if request.method =='POST':
+        message=request.form['message']
+        data=[message]
+        vect=clf.fit_transform(data).toarray()
+        my_prediction=model.predict(vect)
+    return render_template('result.html', prediction=my_prediction)
+        
+        
+        
+        
+if __name__=='__main__':
+    app.run(debug=True)
+        
+
+
+# In[ ]:
+
+
+
+
